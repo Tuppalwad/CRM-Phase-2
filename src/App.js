7b04581bd5cc2f1dc112f64882f13174df397d77
govind-deshmukh <govinddeshmukh2001@gmail.com>
2023-02-14 15:38:15 +0530

    MUI added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
+import Home from './components/landing/index';
+import Forgot from './components/auth/forgot';
+import { BrowserRouter as Router, Routes , Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path='/forgot' element={<Forgot />} />
+          {/* <Route path="*" element={<h1>404 Not Found</h1>} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
